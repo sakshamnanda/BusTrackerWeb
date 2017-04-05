ab1704c0094d932f72824cd24e1bc92704c88966
Chris O'Beirne <cobeirne@deakin.edu.au>
2017-04-05 22:36:21 +1000

    Disable default API requests.

--- BusTrackerWeb/Controllers/ValuesController.cs
+++ BusTrackerWeb/Controllers/ValuesController.cs
@@ -10,31 +10,33 @@ namespace BusTrackerWeb.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET api/values/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
+        //// POST api/values
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
+
+        
     
         /// <summary>
         /// This function updates the current GPS location of a bus running a specific route.
