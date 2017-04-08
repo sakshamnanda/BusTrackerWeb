507b720bfecc10aad492161ea08e1b89641ecbd7
Chris O'Beirne <cobeirne@deakin.edu.au>
2017-04-09 00:21:25 +1000

    Add PutBusLocation action.

---
+++ BusTrackerWeb/Controllers/BusController.cs
@@ -0,0 +1,29 @@
+﻿using BusTrackerWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace BusTrackerWeb.Controllers
+{
+    public class BusController : ApiController
+    {
+        /// <summary>
+        /// Update the longitude and latitude of a bus.
+        /// </summary>
+        /// <param name="bus">The bus to be updated.</param>
+        /// <returns>200 if updated.</returns>
+        [ActionName("PutBusLocation")]
+        public int PutBusLocation([FromBody]BusModel bus)
+        {
+            Debug.WriteLine("PUT api/BusController: BusId={0}, BusRegoNumber={1}, BusLongitude={2}, BusLongitutde={3}",
+                bus.BusId, bus.BusRegoNumber, bus.BusLongitude, bus.BusLatitude);
+
+            return 200;
+        }
+    }
+}
---
+++ BusTrackerWeb/Models/BusModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.ModelBinding;
+
+namespace BusTrackerWeb.Models
+{
+    public class BusModel
+    {
+        public int BusId { get; set; }
+
+        public string BusRegoNumber { get; set; }
+
+        public decimal BusLongitude { get; set; }
+
+        public decimal BusLatitude { get; set; }
+
+        public int BusPreviousStopId { get; set; }
+    }
+}
