21cd60370ebacd0180ba74b428f6b34b812f72ec
Chris O'Beirne <cobeirne@deakin.edu.au>
2017-04-08 21:46:32 +1000

    Fix git ignore bug.

---
+++ BusTrackerWeb/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BusTrackerWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
