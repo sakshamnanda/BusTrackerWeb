8289801e76820e7d84979f6f3119cde1654128b3
Chris O'Beirne <cobeirne@deakin.edu.au>
2017-04-26 21:37:59 +1000

    Update Bus Action Names.

--- BusTrackerWeb/Controllers/BusController.cs
+++ BusTrackerWeb/Controllers/BusController.cs
@@ -14,19 +14,75 @@ namespace BusTrackerWeb.Controllers
     /// Controls all Bus Tracker API Bus action requests.
     /// </summary>
     public class BusController : ApiController
-    {
+    {       
         /// <summary>
         /// Update the longitude and latitude of a bus.
         /// </summary>
         /// <param name="bus">The bus object to be updated.</param>
         /// <returns>200 if updated.</returns>
-        [ActionName("PutBusLocation")]
-        public int PutBusLocation([FromBody]BusModel bus)
+        [ActionName("PutBusOnRouteLocation")]
+        public int PutBusOnRouteLocation([FromBody]BusModel bus)
         {
-            Debug.WriteLine("PUT api/BusController: BusId={0}, BusRegoNumber={1}, BusLongitude={2}, BusLongitutde={3}",
-                bus.BusId, bus.BusRegoNumber, bus.BusLongitude, bus.BusLatitude);
+            // Set default HTTP return code.
+            int httpResult = 400;
 
-            return 200;
+            try
+            {
+                // Check for the bus in the current collection
+                if (WebApiApplication.TrackedBuses.Exists(b => b.BusRegoNumber == bus.BusRegoNumber))
+                {
+                    // If exists, update the bus location.
+                    BusModel trackedBus = WebApiApplication.TrackedBuses.
+                        Single(b => b.BusRegoNumber == bus.BusRegoNumber);
+                    trackedBus.BusLatitude = bus.BusLatitude;
+                    trackedBus.BusLongitude = bus.BusLongitude;
+                    trackedBus.RouteId = bus.RouteId;
+                }
+                else
+                {
+                    // If new, add the new bus to the collection.
+                    BusModel newBus = new BusModel
+                    {
+                        BusRegoNumber = bus.BusRegoNumber,
+                        BusLatitude = bus.BusLatitude,
+                        BusLongitude = bus.BusLongitude,
+                        RouteId = bus.RouteId
+                    };
+
+                    WebApiApplication.TrackedBuses.Add(newBus);
+                }
+
+                httpResult = 200;
+            }
+            catch(Exception e)
+            {
+                Trace.TraceError("PutBusOnRouteLocation Exception: {0}", e.Message);
+            }
+
+            return httpResult;
+        }
+
+        /// <summary>
+        /// Get all buses that are curretly on the defined route.
+        /// </summary>
+        /// <param name="routeId">The PTV Route Id of the buses to be returned.</param>
+        /// <returns>A collection of Bus Model objects.</returns>
+        [ActionName("GetBusOnRouteLocation")]
+        public List<BusModel> GetBusOnRouteLocation(int routeId)
+        {
+            List<BusModel> busesOnRoute = new List<BusModel>();
+
+            try
+            {
+                busesOnRoute = WebApiApplication.TrackedBuses.Where(b => b.RouteId == routeId).
+                    ToList();
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("GetBusOnRouteLocation Exception: {0}", e.Message);
+            }
+             
+            return busesOnRoute;
         }
     }
 }
