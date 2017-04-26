d647b66014be3198fa898eace51dde667d46f98a
Chris O'Beirne <cobeirne@deakin.edu.au>
2017-04-26 22:35:40 +1000

    Add Passenger and Driver views.

---
+++ BusTrackerWeb/Controllers/DriverController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusTrackerWeb.Controllers
+{
+    /// <summary>
+    /// Controls all Bus Tracker Driver View actions.
+    /// </summary>
+    public class DriverController : Controller
+    {
+        /// <summary>
+        /// Open the Passenger Landing View.
+        /// </summary>
+        /// <returns>Passenger View.</returns>
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Driver Landing Page";
+
+            return View();
+        }
+    }
+}
---
+++ BusTrackerWeb/Controllers/PassengerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusTrackerWeb.Controllers
+{
+    /// <summary>
+    /// Controls all Bus Tracker Passenger View actions.
+    /// </summary>
+    public class PassengerController : Controller
+    {
+        /// <summary>
+        /// Open the Passenger Landing View.
+        /// </summary>
+        /// <returns>Passenger View.</returns>
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Passenger Landing Page";
+
+            return View();
+        }
+    }
+}
